0d9334aac41db4717a96c62062ed382de4490edb
Param Shah <paramshah09@gmail.com>
2020-08-03 17:16:25 +0530

    Updates

---
+++ precache-manifest.7e738b1b1ff5340a3400603516956701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c6f58a06b8b46e476c4de8c25d70f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8708ab77cffebd8bfd10",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea658fb4f1f570dac41a",
+    "url": "/static/js/2.8f957717.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f957717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8708ab77cffebd8bfd10",
+    "url": "/static/js/main.167bdea0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
